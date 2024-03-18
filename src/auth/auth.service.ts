98c2ef98096fa5b76ce9256112fd1684c5226d99
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-03-18 15:50:06 -0300

    Refactor login methods and add StudentDto class

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,52 +2,57 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { userInfo } from 'os';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginUserDto, LoginUserStudentDto } from './dto/user.dto';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class AuthService {
 
 
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) { }
 
-  async login(data: LoginUserStudentDto) {
-     const userInfo = await this.prisma.student.findFirst({
-        where: {
-            email_address: data.email_address
-  }
-    });
-   
-    if (!userInfo) {
-      new NotFoundException('User not found');
+    async login(data: LoginUserStudentDto) {
+        const userInfo = await this.prisma.student.findFirst({
+            where: {
+                email_address: data.email_address
+            }
+        });
+
+        if (!userInfo) {
+            new NotFoundException('User not found');
+        }
+
     }
 
-  }
-
-  async loginParent(data: LoginUserDto) {
-    const user = await this.prisma.parent.findFirst({
-        where: {
-            email_address: data.email,
-            password: data.password
-        }
+    async loginParent(data: LoginUserDto) {
+        const user = await this.prisma.parent.findFirst({
+            where: {
+                email_address: data.email,
+                password: data.password
+            }
         });
 
         if (!user) {
             throw new NotFoundException('Parent not found');
-          }
-  }
-
-
-  async loginStudent(data: LoginUserDto) {
-    const user = await this.prisma.student.findFirst({
-        where: {
-            email_address: data.email,
-            password: data.password
         }
+    }
+
+
+    async loginStudent(data: LoginUserDto) {
+        const user = await this.prisma.student.findFirst({
+            where: {
+                email_address: data.email,
+                password: data.password
+            }
         });
 
         if (!user) {
             throw new NotFoundException('Student not found');
-          }
-  }
+        }
+
+        const userDto = plainToClass(LoginUserDto, user);
+
+        return userDto;
+    }
 
 
 }
--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -16,4 +16,46 @@ export class LoginUserDto{
     password: string;
 }
 
+export class StudentDto{
+    @ApiProperty()
+    @IsString()
+    title: string;
+    @ApiProperty()
+    @IsString()
+    forename: string;
+    @ApiProperty()
+    @IsString()
+    surname: string;
+    @ApiProperty()
+    @IsString()
+    middle_name: string;
+    @ApiProperty()
+    @IsString()
+    initials: string;
+    @ApiProperty()
+    @IsString()
+    preferred_name: string;
+    @ApiProperty()
+    @IsString()
+    fullname: string;
+    @ApiProperty()
+    @IsString()
+    gender: string;
+    @ApiProperty()
+    @IsString()
+    form: string;
+    @ApiProperty()
+    @IsString()
+    email_address: string;
+    @ApiProperty()
+    @IsString()
+    pupil_type: string;
+    @ApiProperty()
+    @IsString()
+    enrolment_date: string;
+    @ApiProperty()
+    @IsString()
+    enrolment_school_year: string;
+}
+
 
