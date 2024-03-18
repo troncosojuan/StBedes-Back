df37ecd882ad507068f1fc7bc27aad0fe14e623b
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-03-18 15:13:02 -0300

    Change HTTP method in login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
@@ -8,7 +8,7 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get("login")
+  @Post("login")
   @ApiResponse({ status: 200, description: "User logged in successfully" })
   async login(@Body() data: string) {
     return await this.authService.login(data);
