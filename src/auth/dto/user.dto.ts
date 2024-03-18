47f100bc78c49dce1407cbb1099fb182fb8b4219
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-03-18 15:21:51 -0300

    Add email_address validation to LoginUserStudentDto

--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class LoginUserStudentDto {
+    @ApiProperty()
+    @IsString()
     email_address: string;
 }
