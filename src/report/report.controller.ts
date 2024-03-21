f5c6171060a739ae5c0b5300de1bea44f7e290bb
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-03-21 11:02:49 -0300

    Refactor report controller routes

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-import { Response } from 'express'; // Import the Response type from the express package
 import { ReportService } from './report.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
@@ -9,21 +8,21 @@ export class ReportController {
   constructor(private readonly reportService: ReportService) { }
 
 
-  @Get("get-subject-report")
+  @Get("get-whole-college-report")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
   async getWholeSubjectReport() {
     return await this.reportService.getWholeCollegeResponses();
   }
 
-  @Get("get-teacher-report")
+  @Get("get-subject-report")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
-  async getWholeTeacherReport(@Param("id") id: number) {
+  async getSubjectReport(@Param("id") id: number) {
     return await this.reportService.getSubjectReport(id);
   }
 
   @Get("get-subject-report-by-subject/:id")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
-  async getSubjectReport(@Param("id") id: number) {
+  async getSubjectReportBySubject(@Param("id") id: number) {
     return await this.reportService.getSubjectReportWithTeacher(id);
   }
 
