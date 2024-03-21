8bd940dda09a63dc2164b5b8150f373643b930f0
Juan Troncoso <troncosojuanbernabe@gmail.com>
2024-03-21 10:38:51 -0300

    Refactor report controller methods

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -6,35 +6,38 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags('Report')
 @Controller('report')
 export class ReportController {
-  constructor(private readonly reportService: ReportService) {}
+  constructor(private readonly reportService: ReportService) { }
 
 
   @Get("get-subject-report")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
-  async getWholeSubjectReport() { // Use the Response type for the res parameter
-    return this.reportService.getWholeCollegeResponses();
+  async getWholeSubjectReport() {
+    return await this.reportService.getWholeCollegeResponses();
+  }
+
+  @Get("get-teacher-report")
+  @ApiResponse({ status: 200, description: "Report generated successfully" })
+  async getWholeTeacherReport(@Param("id") id: number) {
+    return await this.reportService.getSubjectReport(id);
   }
 
   @Get("get-subject-report-by-subject/:id")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
-  async getSubjectReport(@Param("id") id:number ) { // Use the Response type for the res parameter
-    const subjectReport = await this.reportService.getSubjectReport(id);
-    const subjectReportWithTeacher = await this.reportService.getSubjectReportWithTeacher(id);
+  async getSubjectReport(@Param("id") id: number) {
+    return await this.reportService.getSubjectReportWithTeacher(id);
+  }
 
-    return {subjectReport, subjectReportWithTeacher};
+  @Get("get-teacher-report")
+  @ApiResponse({ status: 200, description: "Report generated successfully" })
+  async getTeacherReport(@Param("id") id: number) {
+    return await this.reportService.getTeacherReport(id)
   }
 
 
   @Get("get-teacher-report-by-teacher/:id")
   @ApiResponse({ status: 200, description: "Report generated successfully" })
-  async getTeacherReport(@Param("id") id:number) { // Use the Response type for the res parameter
-    const teacherReport = await this.reportService.getTeacherReport(id);
-    const teacherReportWithSubject = await this.reportService.getTeacherReportWithSubject(id);
-
-    return {teacherReport ,teacherReportWithSubject};
+  async getTeacherReportByTeacher(@Param("id") id: number) {
+    return await this.reportService.getTeacherReportWithSubject(id);
   }
 
-  
-  
-  
 }
