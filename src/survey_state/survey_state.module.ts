ea59378d9c08d34939ea4ddef5d01008fd35a6e6
nmorerosyf <136317686+nmorerosyf@users.noreply.github.com>
2024-05-29 15:37:45 -0300

    added prisma service on module

--- src/survey_state/survey_state.module.ts
+++ src/survey_state/survey_state.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SurveyStateController } from './survey_state.controller';
 import { SurveyStateService } from './survey_state.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [SurveyStateController],
-  providers: [SurveyStateService]
+  providers: [SurveyStateService, PrismaService]
 })
 export class SurveyStateModule {}
